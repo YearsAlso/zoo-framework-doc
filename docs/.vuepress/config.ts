3a7151261077a1378d121182725ff38697c9e19d
YearsAlso <1334713380@qq.com>
2023-08-05 19:44:33 +0800

    init

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,30 +1,42 @@
-import { defineUserConfig } from 'vuepress'
-import { defaultTheme } from 'vuepress'
+import {defineUserConfig} from 'vuepress'
+import {defaultTheme} from 'vuepress'
 
 
-export default defineUserConfig({ 
-theme: defaultTheme({
+export default defineUserConfig({
+    title: 'Zoo Framework',
+    description: '',
+    theme: defaultTheme({
+        locales: {
+            '/': {
+                selectLanguageName: 'English',
+            },
+            '/zh/': {
+                selectLanguageName: '简体中文',
+
+            },
+        },
+        sidebar: [{
+            text: '快速开始',
+            link: '/start/',
+            children: [
+                // SidebarItem
+                // 字符串 - 页面文件路径
+                '/start/new.md',
+            ],
+        },]
+    }),
     locales: {
+        // 键名是该语言所属的子路径
+        // 作为特例，默认语言可以使用 '/' 作为其路径。
         '/': {
-        selectLanguageName: 'English',
+            lang: 'zh-CN',
+            title: 'Zoo Framework',
+            description: 'Vue-powered Static Site Generator',
         },
         '/zh/': {
-        selectLanguageName: '简体中文',
+            lang: 'zh-CN',
+            title: 'Zoo Framework',
+            description: 'Vue 驱动的静态网站生成器',
         },
     },
-    }),
-  locales: {
-    // 键名是该语言所属的子路径
-    // 作为特例，默认语言可以使用 '/' 作为其路径。
-    '/': {
-      lang: 'en-US',
-      title: 'VuePress',
-      description: 'Vue-powered Static Site Generator',
-    },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: 'VuePress',
-      description: 'Vue 驱动的静态网站生成器',
-    },
-  },
 })
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+
+
+# Zoo Framework 
+
+Zoo Framework 是一款响应式多线程开发框架，开发目的是为减少python 项目开发中多线程带来的痛点，如反复创建Thread
+对象，同步方法阻塞与异步问题
